24e3a43ce4e8399a564e90ffe8427ebb343cb6ea
xiaowei <xiaowei20150509YIUHLK>
2021-08-05 15:13:15 +0800

    新增分裂器，实现内部窗口之间缩放

--- frameLessScale/frameLessScale.cpp
+++ frameLessScale/frameLessScale.cpp
@@ -6,6 +6,25 @@ frameLessScale::frameLessScale(QWidget *parent)
 {
     ui.setupUi(this);
 	setWindowFlags(Qt::FramelessWindowHint);
+
+	//分裂器
+		//设置各个分裂窗口显示比例
+	ui.splitter_main->setStretchFactor(0, 3);
+	ui.splitter_main->setStretchFactor(1, 3);
+	ui.splitter_main->setStretchFactor(2, 1);
+
+	ui.splitter->setStretchFactor(0, 1);
+	ui.splitter->setStretchFactor(1, 4);
+	ui.splitter->setStretchFactor(2, 1);
+
+	//拖拉分割线的时候，窗口是否实时显示
+	ui.splitter->setOpaqueResize(false);
+	// 不让子控件被压缩到大小为0
+	ui.splitter->setChildrenCollapsible(false);
+	//可能分裂窗口内还有一些控件，为了保证其内部控件跟随变动（未布局的情况下），可以通过QSplitter的splitterMoved信号去调用那些控件resize
+	connect(ui.splitter, &QSplitter::splitterMoved, this, [this]() {
+		//xx(); 某种需求功能接口
+	});
 }
 
 frameLessScale::~frameLessScale()
