f0392da77947f09ac2b5b1f5a0404e03cf610cfa
xiaowei <xiaowei20150509YIUHLK>
2021-09-15 14:21:39 +0800

    边框阴影扩散动画

--- animation/animationTest.cpp
+++ animation/animationTest.cpp
@@ -1,6 +1,7 @@
 #include "animationTest.h"
 #include <QPropertyAnimation>
 #include "popAnimation.h"
+#include <QGraphicsDropShadowEffect>
 
 animationTest::animationTest(QWidget *parent)
     : QWidget(parent)
@@ -10,23 +11,35 @@ animationTest::animationTest(QWidget *parent)
 	m_pAnimation = new QPropertyAnimation(ui.label, "pos");
 	if (m_pAnimation) {
 		m_pAnimation->setDuration(2000);
-		m_pAnimation->setStartValue(ui.label->pos());
-		m_pAnimation->setEndValue(QPoint(-(ui.label->width()), ui.label->y()));
+		//m_pAnimation->setLoopCount(-1);
+		connect(m_pAnimation, &QVariantAnimation::finished, this, &animationTest::sltMoveChanged);
 		//m_pAnimation->setEasingCurve(QEasingCurve::OutBounce);  // 缓和曲线风格
 	}
 
-	//m_pOpacityAnimation = new QPropertyAnimation(ui.label, "windowOpacity");
-	//if (m_pOpacityAnimation) {
-	//	m_pOpacityAnimation->setDuration(1000);
-	//	m_pOpacityAnimation->setStartValue(1);
-	//	m_pOpacityAnimation->setEndValue(0);
-	//	connect(m_pOpacityAnimation, &QPropertyAnimation::finished, ui.label, &QLabel::hide);
-	//}
+	m_vShadowEffectAnimation = new QVariantAnimation(this);
+	if (m_vShadowEffectAnimation) {
+		m_vShadowEffectAnimation->setDuration(2000);
+		m_vShadowEffectAnimation->setKeyValueAt(0, 0);
+		m_vShadowEffectAnimation->setKeyValueAt(0.5, 100);
+		m_vShadowEffectAnimation->setKeyValueAt(1, 0);
+		m_vShadowEffectAnimation->setLoopCount(-1);
+		connect(m_vShadowEffectAnimation, &QVariantAnimation::valueChanged, this, &animationTest::sltShadowEffectRaduisChanged);
+	}
+
+	m_shadowEffect = new QGraphicsDropShadowEffect(this);
+	//设置边框阴影
+	if (m_shadowEffect) {
+		m_shadowEffect->setColor(QColor(0, 0, 139));
+		m_shadowEffect->setOffset(0, 0);
+		m_shadowEffect->setBlurRadius(0);
+		ui.label_shadowEffect->setGraphicsEffect(m_shadowEffect);
+	}
 	
 	connect(ui.pushButton, &QPushButton::clicked, this, &animationTest::sltStartPopAnimation);
 	connect(ui.pushButton2, &QPushButton::clicked, this, &animationTest::sltStartHideAnimation);
 	connect(ui.pushButton3, &QPushButton::clicked, this, &animationTest::sltStartOpacityAnimation);
 	connect(ui.pushButton4, &QPushButton::clicked, ui.label_rotation, &rotationLabel::startRotationAnimation);
+	connect(ui.pushButton5, &QPushButton::clicked, this, &animationTest::sltStartShadowEffectAnimation);
 }
 
 animationTest::~animationTest()
@@ -38,20 +51,23 @@ void animationTest::sltStartHideAnimation()
 	if (Q_NULLPTR == m_pAnimation)
 		return;
 
-	if(QAbstractAnimation::Running == m_pAnimation->state())
+	if (QAbstractAnimation::Running == m_pAnimation->state()) {
+		m_pAnimation->pause();
 		return;
+	}
+	else if (QAbstractAnimation::Paused == m_pAnimation->state()) {
+		m_pAnimation->start();
+		return;
+	}
+
+	m_pAnimation->setStartValue(QPoint(768, 640));
+	m_pAnimation->setEndValue(QPoint(247, ui.label->y()));
 
 	m_pAnimation->start();
 }
 
 void animationTest::sltStartOpacityAnimation()
 {
-	//if (Q_NULLPTR == m_pOpacityAnimation)
-	//	return;
-
-	//if (QAbstractAnimation::Running == m_pOpacityAnimation->state())
-	//	return;
-
 	QWidget *widget = new QWidget();
 	if (widget) {
 		widget->setAttribute(Qt::WA_DeleteOnClose);
@@ -67,6 +83,54 @@ void animationTest::sltStartOpacityAnimation()
 			connect(pOpacityAnimation, &QPropertyAnimation::finished, widget, &QWidget::close);
 		}
 	}
+}
+
+void animationTest::sltStartShadowEffectAnimation()
+{
+	if (Q_NULLPTR == m_vShadowEffectAnimation)
+		return;
+
+	if (QAbstractAnimation::Running == m_vShadowEffectAnimation->state()) {
+		m_vShadowEffectAnimation->pause();
+		return;
+	}
+
+	m_vShadowEffectAnimation->start();
+}
+
+void animationTest::sltShadowEffectRaduisChanged(QVariant value)
+{
+	auto radius = value.toInt();
+	if (m_shadowEffect) {
+		m_shadowEffect->setBlurRadius(radius);
+	}
+}
+
+void animationTest::sltMoveChanged()
+{
+	//判断方位
+	auto posX = ui.label->x();
+	auto posY = ui.label->y();
+	m_pAnimation->setStartValue(ui.label->pos());
+	//向上移动
+	if (247 == posX && 640 == posY) {
+		m_pAnimation->setEndValue(QPoint(posX, 120));
+	}
+	//向右移动
+	if (247 == posX && 120 == posY) {
+		m_pAnimation->setEndValue(QPoint(768, posY));
+	}
+	//向下移动
+	if (768 == posX && 120 == posY) {
+		m_pAnimation->setEndValue(QPoint(posX, 640));
+	}
+	//向左移动
+	if (768 == posX && 640 == posY) {
+		m_pAnimation->setEndValue(QPoint(247, posY));
+	}
+
+	if (QAbstractAnimation::Stopped == m_pAnimation->state())
+		m_pAnimation->start();
 }
 
 void animationTest::sltStartPopAnimation()
--- animation/animationTest.h
+++ animation/animationTest.h
@@ -4,6 +4,7 @@
 #include "ui_animationTest.h"
 
 class QPropertyAnimation;
+class QGraphicsDropShadowEffect;
 
 class animationTest : public QWidget
 {
@@ -17,11 +18,16 @@ private slots:
 	void sltStartPopAnimation();
 	void sltStartHideAnimation();
 	void sltStartOpacityAnimation();
+	void sltStartShadowEffectAnimation();
+	void sltShadowEffectRaduisChanged(QVariant value);
+	void sltMoveChanged();
 
 private:
     Ui::animationTestClass ui;
 	//动画特效
 	QPropertyAnimation *m_pAnimation = Q_NULLPTR;
-	//窗口关闭动画特效
-	QPropertyAnimation *m_pOpacityAnimation = Q_NULLPTR;
+	//阴影类
+	QGraphicsDropShadowEffect *m_shadowEffect = Q_NULLPTR;
+	//阴影控制特效
+	QVariantAnimation *m_vShadowEffectAnimation = Q_NULLPTR;
 };
