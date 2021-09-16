0f189cb47e40a9e4871767d0e5228eb5abc160b5
xiaowei <xiaowei20150509YIUHLK>
2021-09-16 15:19:27 +0800

    移除多余文件

--- heartProgress/main.cpp
+++ heartProgress/main.cpp
@@ -1,12 +1,10 @@
 #include "heartProgress.h"
-#include "WaterProgressBarTest.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    /*heartProgress w;*/
-	WaterProgressBarTest w;
+    heartProgress w;
     w.show();
     return a.exec();
 }
